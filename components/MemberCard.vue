09ce3e4aef28bf85267907ee8545c75115b66f37
PaulMagos <paulmagos@icloud.com>
2024-01-14 04:50:07 +0000

    See https://github.com/SmartReports/smartreports-landing/commit/94c63b21311edb58d5feeeea960e626a80206c07 from refs/heads/main

--- components/MemberCard.vue
+++ components/MemberCard.vue
@@ -42,7 +42,7 @@ export default defineComponent({
       return this.user.local_groups
     },
     local_avatar() {
-      return `@/assets/imgs/${this.user.local_avatar}`
+      return `./${this.user.local_avatar}`
     },
   },
 });
